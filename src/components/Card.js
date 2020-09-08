d028c6aa9055f234cf47e9c9f6eaa47b0f4cf83a
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-09-08 09:41:58 +0800

    finalize card and head component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLocation } from "../contexts/LocationContext";
+import Loader from "./Loader";
 
 export default function Card() {
     const { location } = useLocation();
@@ -10,7 +11,7 @@ export default function Card() {
             value: location?.ip,
         },
         {
-            label: "LOCATION?",
+            label: "LOCATION",
             value: `${location?.location?.city},${location?.location?.region} ${location?.location?.postalCode}`,
         },
         {
@@ -23,20 +24,22 @@ export default function Card() {
         },
     ];
 
-    return !location ? (
-        "Loading..."
-    ) : (
+    return (
         <ul className="relative z-10 bg-white rounded-lg shadow-lg text-gray-900 w-90 mx-auto px-4 py-6 text-center bg-cover bg-center md:grid md:grid-flow-col md:gap-4 md:px-8 md:min-h-ten md:items-center md:w-4/5">
-            {details?.map((item) => (
-                <li className="mb-3 md:text-left md:mb-0" key={item.label}>
-                    <h3 className="text-xs text-gray-500 md:text-md">
-                        {item.label}
-                    </h3>
-                    <p className="text-md font-semibold md:text-xl">
-                        {item.value}
-                    </p>
-                </li>
-            ))}
+            {!location ? (
+                <Loader />
+            ) : (
+                details?.map((item) => (
+                    <li className="mb-3 md:text-left md:mb-0 md:border-l-2 border-gray-400 pl-4 first:border-l-0" key={item.label}>
+                        <h3 className="text-xs text-gray-500 md:text-md">
+                            {item.label}
+                        </h3>
+                        <p className="text-md font-semibold md:text-xl">
+                            {item.value}
+                        </p>
+                    </li>
+                ))
+            )}
         </ul>
     );
 }
--- src/components/Head.js
+++ src/components/Head.js
@@ -4,12 +4,12 @@ import Card from "./Card";
 
 export default function Head() {
     return (
-        <div className='h-40 py-8 bg-pattern'>
+        <div className="h-40 py-8 bg-pattern">
             <h1 className="text-center font-normal mb-4 text-2xl text-white">
                 IP Address Tracker
             </h1>
-            <Search/>
-            <Card/>
+            <Search />
+            <Card />
         </div>
     );
 }
